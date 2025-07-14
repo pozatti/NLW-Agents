baa5c45b11469ce9c2b84b45c0650da70e210aea
pozatti <andrepozatti@outlook.com>
2025-07-14 08:38:58 -0300

    feat: web and server

---
+++ server/src/server.ts
@@ -0,0 +1,32 @@
+import { fastifyCors } from '@fastify/cors'
+import { fastify } from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { env } from './env.ts'
+import { createQuestionRoute } from './http/routes/create-question.ts'
+import { createRoomsRoute } from './http/routes/create-rooms.ts'
+import { getRoomsQuestions } from './http/routes/get-room-questions.ts'
+import { getRoomsRoute } from './http/routes/get-rooms.ts'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+  origin: 'http://localhost:5173',
+})
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.get('/health', () => {
+  return 'OK'
+})
+
+app.register(getRoomsRoute)
+app.register(createRoomsRoute)
+app.register(getRoomsQuestions)
+app.register(createQuestionRoute)
+
+app.listen({ port: env.PORT })
